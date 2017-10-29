e16de06c90a88e6feb1542d70cf21664d25ca3e0
Ben <benclerc74@gmail.com>
2017-10-29 11:30:06 +0100

    Added Slim

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+
+require 'vendor/autoload.php';
+
+$app = new Slim\App();
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->write("Hello, " . $args['name']);
+});
+
+$app->run();
+
+?>
+
+<?php
 // Including quotes file
 include 'quotes.php';
 

760be01218e45e7dec55ea23084f9a9d05179a70
Ben <benclerc74@gmail.com>
2017-10-29 14:22:34 +0100

    Adding mode 'all' and remove old code

--- index.php
+++ index.php
@@ -8,77 +8,92 @@ include 'quotes.php';
 // Declaring the router
 $router = new Slim\App();
 
-// Test
-$router->get('/hello/{name}', function ($request, $response, $args) {
-	return $response->write("Hello, " . $args['name']);
-});
-
 // Random quotes with or without a season or a character wanted
-$router->map(['GET', 'POST'], '/api/random[/{arg1}[/{arg2}]]', function ($request, $response, $args) {
-	global $haystackQuotes;
-	// With a season and a character
-	if (!empty($args['arg1']) && !empty($args['arg2'])) {
-		$params = getParams1($args['arg1'], $args['arg2']);
-		// Checking if the given args are correct
-		if (empty($params['error'])) {
-			// Creating the array with all possibles quotes
-			foreach ($haystackQuotes as $key => $value) {
-				if ($value['infos']['personnage'] == $params['personnage'] && $value['infos']['saison'] == mapLivre($params['livre'])) {
-					$quotes[] = $value;
-				}
-			}
-			// Creating the result
-			if (!empty($quotes)) {
-				$return['status'] = 1;
-				$return['citation'] = $quotes[array_rand($quotes)];
-			} else {
-				$return['status'] = 0;
-				$return['error'] = 'Aucun resultat';
-			}
-		} else {
-			$return['status'] = 0;
-			$return['error'] = $params['error'];
-		}
-
-	// With a season or a character wanted
-	} elseif (!empty($args['arg1'])) {
-		$params = getParams2($args['arg1']);
-		// Checking if the given arg is correct
-		if (empty($params['error'])) {
-			// If the arg is a season
-			if (!empty($params['livre'])) {
-				foreach ($haystackQuotes as $key => $value) {
-					if ($value['infos']['saison'] == mapLivre($params['livre'])) {
-						$quotes[] = $value;
-					}
-				}
-			// If the arg is a character
-			} elseif (!empty($params['personnage'])) {
+$router->map(['GET', 'POST'], '/api/{mode}[/{arg1}[/{arg2}]]', function ($request, $response, $args) {
+	if ($args['mode'] == 'random' || $args['mode'] == 'all') {
+		global $haystackQuotes;
+		// With a season and a character
+		if (!empty($args['arg1']) && !empty($args['arg2'])) {
+			$params = getParams1($args['arg1'], $args['arg2']);
+			// Checking if the given args are correct
+			if (empty($params['error'])) {
 				// Creating the array with all possibles quotes
 				foreach ($haystackQuotes as $key => $value) {
-					if ($value['infos']['personnage'] == $params['personnage']) {
+					if ($value['infos']['personnage'] == $params['personnage'] && $value['infos']['saison'] == mapLivre($params['livre'])) {
 						$quotes[] = $value;
 					}
 				}
-			}
-			// Creating the result
-			if (!empty($quotes)) {
-				$return['status'] = 1;
-				$return['citation'] = $quotes[array_rand($quotes)];
+				// Creating the result
+				if (!empty($quotes)) {
+					$return['status'] = 1;
+					// If mode random then get only one quote, else if it's all mode get all the quotes 
+					if ($args['mode'] == 'random') {
+						$return['citation'] = $quotes[array_rand($quotes)];
+					} else {
+						$return['citations'] = $quotes;
+					}
+				} else {
+					$return['status'] = 0;
+					$return['error'] = 'Aucun resultat';
+				}
 			} else {
 				$return['status'] = 0;
-				$return['error'] = 'Aucun resultat';
+				$return['error'] = $params['error'];
 			}
-		} else {
-			$return['status'] = 0;
-			$return['error'] = $params['error'];
-		}
 
-	// With nothing wanted, only a random quote
+		// With a season or a character wanted
+		} elseif (!empty($args['arg1'])) {
+			$params = getParams2($args['arg1']);
+			// Checking if the given arg is correct
+			if (empty($params['error'])) {
+				// If the arg is a season
+				if (!empty($params['livre'])) {
+					foreach ($haystackQuotes as $key => $value) {
+						if ($value['infos']['saison'] == mapLivre($params['livre'])) {
+							$quotes[] = $value;
+						}
+					}
+				// If the arg is a character
+				} elseif (!empty($params['personnage'])) {
+					// Creating the array with all possibles quotes
+					foreach ($haystackQuotes as $key => $value) {
+						if ($value['infos']['personnage'] == $params['personnage']) {
+							$quotes[] = $value;
+						}
+					}
+				}
+				// Creating the result
+				if (!empty($quotes)) {
+					$return['status'] = 1;
+					// If mode random then get only one quote, else if it's all mode get all the quotes 
+					if ($args['mode'] == 'random') {
+						$return['citation'] = $quotes[array_rand($quotes)];
+					} else {
+						$return['citations'] = $quotes;
+					}
+				} else {
+					$return['status'] = 0;
+					$return['error'] = 'Aucun resultat';
+				}
+			} else {
+				$return['status'] = 0;
+				$return['error'] = $params['error'];
+			}
+
+		// With nothing wanted, only a random quote
+		} else {
+			// Getting a random quotes without argument
+			$return['status'] = 1;
+			// If mode random then get only one quote, else if it's all mode get all the quotes 
+			if ($args['mode'] == 'random') {
+				$return['citation'] = $haystackQuotes[array_rand($haystackQuotes)];
+			} else {
+				$return['citations'] = $haystackQuotes;
+			}
+		}
 	} else {
-		// Getting a random quotes without argument
-		$return['status'] = 1;
-		$return['citation'] = $haystackQuotes[array_rand($haystackQuotes)];
+		$return['status'] = 0;
+		$return['error'] = 'Mode de citation incorrect ("random" ou "all").';
 	}
 
 	// Printing the answer in JSON format
@@ -88,6 +103,7 @@ $router->map(['GET', 'POST'], '/api/random[/{arg1}[/{arg2}]]', function ($reques
 $router->run();
 
 
+// Functions
 
 function getParams1($first, $second) {
 	global $onlyPersos;
@@ -173,187 +189,5 @@ function mapLivre($n) {
 			break;
 	}
 }
-
-
-
-
-// echo('<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>');
-// var_dump($haystackQuotes);
-
-?>
-
-<?php
-// // Including quotes file
-// include 'quotes.php';
-
-// foreach ($haystackQuotes as $key => $value) {
-
-// 	if ($value['infos']['saison'] == 'Livre I' || $value['infos']['saison'] == 'Livre II' || $value['infos']['saison'] == 'Livre II' || $value['infos']['saison'] == 'Livre IV' || $value['infos']['saison'] == 'Livre V' || $value['infos']['saison'] == 'Livre VI') {
-// 		echo($value['citation'].'<br>');
-// 	}
-// }
-
-// // Getting personnage requested
-// if (isset($_POST['personnage'])) {
-// 	$personnage = $_POST['personnage'];
-// } elseif ($_GET['personnage']) {
-// 	$personnage = $_GET['personnage'];
-// }
-
-// if (isset($personnage)) {
-// 	if (in_array($personnage, $onlyPersos)) {
-// 		foreach ($haystackQuotes as $key => $value) {
-// 			if ($value['infos']['personnage'] == $personnage) { $quotes[] = $value; }
-// 		}
-// 	} else {
-// 		$err = 'Personnage introuvable.';
-// 	}
-// } else {
-// 	$quotes = $haystackQuotes;
-// }
-
-// // Getting mode requested
-// if (isset($_POST['mode'])) { $mode = $_POST['mode'];
-// } elseif (isset($_GET['mode'])) {
-// 	$mode = $_GET['mode'];
-// } else {
-// 	$mode = 1;
-// }
-// if ($mode < 0 || $mode > 4) { $err = 'Mode incoonu.'; }
-
-// // Printing result
-// if (isset($err)) {
-// 	echo($err);
-// } else {
-// 	switch ($mode) {
-// 		case '1':
-// 			echo(modeSimple($quotes));
-// 			break;
-// 		case '2':
-// 			echo(modeComplete($quotes));
-// 			break;
-// 		case '3':
-// 			echo(modeFull($quotes));
-// 			break;
-		
-// 		default:
-// 			echo('Error');
-// 			break;
-// 	}
-// }
-
-
-
-
-
-
-
-
-// // Functions for put in form the quote
-// function modeSimple($array) {
-// 	$key = array_rand($array);
-// 	if (!isset($array[$key]['infos']['personnage'])) { $array[$key]['infos']['personnage'] = 'Inconnu'; }
-// 	return $array[$key]['citation'].' - '.$array[$key]['infos']['personnage'];
-// }
-
-// function modeComplete($array) {
-// 	$key = array_rand($array);
-// 	if (!isset($array[$key]['infos']['personnage'])) { $array[$key]['infos']['personnage'] = 'Inconnu'; }
-// 	if (!isset($array[$key]['infos']['saison'])) { $array[$key]['infos']['saison'] = 'Inconnu'; }
-// 	if (!isset($array[$key]['infos']['episode'])) { $array[$key]['infos']['episode'] = 'Inconnu'; }
-// 	return $array[$key]['citation'].' - '.$array[$key]['infos']['personnage'].' - '.$array[$key]['infos']['saison'].', '.$array[$key]['infos']['episode'];
-// }
-
-// function modeFull($array) {
-// 	$key = array_rand($array);
-// 	if (!isset($array[$key]['infos']['personnage'])) { $array[$key]['infos']['personnage'] = 'Inconnu'; }
-// 	if (!isset($array[$key]['infos']['acteur'])) { $array[$key]['infos']['acteur'] = 'Inconnu'; }
-// 	if (!isset($array[$key]['infos']['saison'])) { $array[$key]['infos']['saison'] = 'Inconnu'; }
-// 	if (!isset($array[$key]['infos']['episode'])) { $array[$key]['infos']['episode'] = 'Inconnu'; }
-// 	if (!isset($array[$key]['infos']['auteur'])) { $array[$key]['infos']['auteur'] = 'Inconnu'; }
-// 	return $array[$key]['citation'].' - '.$array[$key]['infos']['personnage'].' ('.$array[$key]['infos']['acteur'].') - '.$array[$key]['infos']['saison'].', '.$array[$key]['infos']['episode'].' ('.$array[$key]['infos']['auteur'].')';
-// }
-
-
-
-
-// Process to get all the quotes in an array
-// $once = explode('{{citation|citation=', $infos);
-
-// foreach ($once as $keyOnce => $valueOnce) {
-// 	$i = 1;
-// 	foreach (explode('{{Réf Série', $valueOnce) as $keyCitation => $valueCitation) {
-// 		if ($i%2 == 1) {
-// 			$result[$keyOnce]['citation'] = substr($valueCitation, 0, -4);
-// 		} else {
-// 			foreach (explode('|', $valueCitation) as $keyInfo => $valueInfo) {
-// 				$valueInfo = str_replace("'", "", $valueInfo);
-// 				if (strpos($valueInfo, 'acteur') !== FALSE) {
-// 					$result[$keyOnce]['infos']['acteur'] = substr($valueInfo, 7, -2);
-// 					$result[$keyOnce]['infos']['personnage'] = $persos[$result[$keyOnce]['infos']['acteur']];
-// 				} elseif (strpos($valueInfo, 'auteur') !== FALSE) {
-// 					$result[$keyOnce]['infos']['auteur'] = substr($valueInfo, 7, -2);
-// 				} elseif (strpos($valueInfo, 'serie') !== FALSE) {
-// 					$result[$keyOnce]['infos']['serie'] = substr($valueInfo, 7, -2);
-// 				} elseif (strpos($valueInfo, 'saison') !== FALSE) {
-// 					$result[$keyOnce]['infos']['saison'] = substr($valueInfo, 7);
-// 				} elseif (strpos($valueInfo, 'épisode') !== FALSE) {
-// 					$result[$keyOnce]['infos']['episode'] = substr($valueInfo, 9, -6);
-// 				}
-// 			}
-// 		}
-// 		$i++;
-// 	}
-// }
-
-
-// Output php array
-// echo('[');
-// foreach ($result as $key => $value) {
-// 	echo($key.' => [
-// 			\'citation\' => \''.$value['citation'].'\',
-// 		');
-// 		if (isset($value['infos']['serie'])) {
-// 			echo('\'serie\' => \''.$value['infos']['serie'].'\',
-// 			');
-// 		}
-// 		if (isset($value['infos']['saison'])) {
-// 			echo('\'saison\' => \''.$value['infos']['saison'].'\',
-// 			');
-// 		}
-// 		if (isset($value['infos']['episode'])) {
-// 			echo('\'episode\' => \''.$value['infos']['episode'].'\',
-// 			');
-// 		}
-// 		if (isset($value['infos']['auteur'])) {
-// 			echo('\'auteur\' => \''.$value['infos']['auteur'].'\',
-// 			');
-// 		}
-// 		if (isset($value['infos']['acteur'])) {
-// 			echo('\'acteur\' => \''.$value['infos']['acteur'].'\',
-// 			');
-// 		}
-// 		if (isset($value['infos']['personnage'])) {
-// 			echo('\'personnage\' => \''.$value['infos']['personnage'].'\',
-// 			');
-// 		}
-// 		echo(']');
-// }
-// echo(']');
-
-
-// Output all the quotes
-// foreach ($result as $key => $value) {
-// 	echo($value['citation'].'<br>');
-// 	foreach ($value['infos'] as $key2 => $value2) {
-// 		echo('&nbsp;&nbsp;&nbsp;'.$value2.'<br>');
-// 	}
-// 	echo('<br> <br>');
-// }
-
-
-
-
-
 
 ?>

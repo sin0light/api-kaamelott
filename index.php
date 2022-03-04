ff63ae8d7316cccdf03af3e91e708dbcab2c71b2
Benjamin Clerc <contact@benjamin-clerc.com>
2022-03-04 17:16:12 +0100

    Added routes and DB object

--- index.php
+++ index.php
@@ -9,18 +9,27 @@
  * Load composer packages and require controllers files.
  */
 require 'vendor/autoload.php';
+// Config file
+require_once 'config.php';
 // Controllers' files
 require_once 'controller/controllerUtils.php';
-
 
 
 /**
  * Set environement.
  */
+// Start PHP sessions
+session_start();
 // Set local datetime
 date_default_timezone_set('Europe/Paris');
 // Create router
 $router = new \Klein\Klein();
+// Define applications to be used from any controller
+$router->respond(function ($request, $response, $service, $app) {
+	$app->register('db', function() {
+		return new \DataManagement\DataManagement('pgsql', $database['host'], $database['port'], $database['dbname'], $database['user'], $database['password']);
+	});
+});
 
 
 /**
@@ -29,6 +38,28 @@ $router = new \Klein\Klein();
 
 // Redirect to about page in french
 $router->respond('GET', '/', $controllerHome);
+
+// API calls
+// Random quotes
+$router->respond('GET', '/api/random', $controllerRandom);
+// By character
+$router->respond('GET', '/api/random/personnage/[:character]', $controllerRandomCharacter);
+// By season
+$router->respond('GET', '/api/random/livre/[:season]', $controllerRandomSeason);
+// By season and character
+$router->respond('GET', '/api/random/livre/[:season]/personnage/[:character]', $controllerRandomSeasonCharacter);
+
+// All quotes
+$router->respond('GET', '/api/all', $controllerAll);
+// By character
+$router->respond('GET', '/api/all/personnage/[:character]', $controllerAllCharacter);
+// By season
+$router->respond('GET', '/api/all/livre/[:season]', $controllerAllSeason);
+// By season and character
+$router->respond('GET', '/api/all/livre/[:season]/personnage/[:character]', $controllerAllSeasonCharacter);
+
+// Sounds
+$router->respond('GET', '/api/sounds/[:filename]', $controllerSounds);
 
 
 // Catch errors
